cb3b8537682aa58780281f95ad8495cb24fcd357
mtoohey31 <mtoohey31@users.noreply.github.com>
2021-10-13 23:05:51 -0400

    style: format with stylua

--- lua/completion-fish.lua
+++ lua/completion-fish.lua
@@ -7,12 +7,12 @@ function F.getCompletionItems(prefix)
   local colnr = cursor_pos[0]
   local lines = vim.api.nvim_buf_get_lines(0, 0, linenr, false)
   lines[linenr] = lines[linenr]:sub(0, colnr)
-  local script_parts = {'complete --do-complete \''}
+  local script_parts = { "complete --do-complete '" }
   for _, line in ipairs(lines) do
     local escaped_line = line:gsub("\\", "\\\\"):gsub("'", "\\'")
     table.insert(script_parts, escaped_line)
   end
-  local script = table.concat(script_parts, '\n') .. "'"
+  local script = table.concat(script_parts, "\n") .. "'"
   local tmppath = os.tmpname()
   local tmpfile = io.open(tmppath, "w")
   tmpfile:write(script)
@@ -21,16 +21,16 @@ function F.getCompletionItems(prefix)
   local output = pipe:read("*a")
   pipe:close()
   os.remove(tmppath)
-  for completion in vim.gsplit(output, '\n') do
-    local index = completion:find('\t')
+  for completion in vim.gsplit(output, "\n") do
+    local index = completion:find("\t")
     if index ~= nil then
       local word = completion:sub(0, index - 1)
       if vim.startswith(word, prefix) then
         table.insert(complete_items, {
-            word = word,
-            menu = completion:sub(index + 1, completion:len() - 1),
-            kind = "Fish",
-          })
+          word = word,
+          menu = completion:sub(index + 1, completion:len() - 1),
+          kind = "Fish",
+        })
       end
     end
   end
@@ -38,7 +38,7 @@ function F.getCompletionItems(prefix)
 end
 
 F.complete_item = {
-  item = F.getCompletionItems
+  item = F.getCompletionItems,
 }
 
 return F
